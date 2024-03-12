fb6770b3c92d64193ae739542e144138687d9c76
Supasak Kulawonganunchai <ksupasak@gmail.com>
2024-03-12 23:29:58 +0700

    Update translate.py

--- lib/translate.py
+++ lib/translate.py
@@ -3,5 +3,5 @@ import sys
 # text = "ศุภศักดิ์ กุลวงศ์อนันชัย"
 th_name = sys.argv[1]
 
-en_name = romanize(th_name, engine="royin" )
+en_name = romanize(th_name, engine="thai2rom_onnx" )
 print(en_name)
